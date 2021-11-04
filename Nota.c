1acdf671af216b68bd0819b4b41bfb5a474d0496
Leonel <franciscoleonelfds@gmail.com>
2021-11-04 19:36:46 -0300

    Atualuzação

---
+++ Nota.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+int main()
+{
+
+    int NF = 2, i = 1, n_iten = 3, t = 1;
+    float val_item, total, total1;
+    char nome_item[25];
+
+    while (t <= NF)
+    {
+        printf("----------------------------------------------\n");
+        printf("Digite os valores da notas fiscais n: %i \n", t);
+        printf("----------------------------------------------\n");
+
+        while (i <= n_iten)
+        {
+
+            printf("Digite o nome do item: ");
+            scanf("%s", &nome_item);
+            printf("Digite o valor dos itens: ");
+            scanf("%f", &val_item);
+            i++;
+            total += val_item;
+            total1 += val_item;
+        }
+        printf("----------------------------------------------\n");
+        printf("O valor total e: %f \n", total);
+        printf("----------------------------------------------\n");
+        printf("\n");
+        total = 0;
+        t++;
+        i = 1;
+    }
+    printf("----------------------------------------------\n");
+    printf("O valor total de todas as notas fiscais e: %f \n", total1);
+    printf("----------------------------------------------\n");
+
+    return 0;
+}
---
+++ readme.md
@@ -0,0 +1,5 @@
+Olá, essa é minha primeira vez usando o git. Esse arquivo .md terá algumas instruções sobre um código em lingugem C, o qual está no meu primeiro repositório do github.
+
+Anexei um arquivo em C. Esse código em C exibe *n* notas fiscais, que solicita o *n* itens, os quais tem seu respectivos valores que o usuário deverá digitar e logo depois mostra a soma dos itens de cada nota fiscal e a soma de todas as notas fiscais.
+
+Ps: Isso é uma alteração.
